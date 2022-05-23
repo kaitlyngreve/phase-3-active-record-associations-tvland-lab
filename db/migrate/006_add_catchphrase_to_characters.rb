701e5aa07cc044aec81d9f2ed46dbb912a5f10e1
Kaitlyn Greve <kgreve14@gmail.com>
2022-05-23 16:55:04 -0400

    commit tree b5b13531ec54335f576c6b03c94c924e771132ad

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,5 +1,6 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[6.1]
   def change
-    # Your code here
+    add_column :characters, :catchphrase, :string
+    #the table you want to add to, the name of the column, and the data type
   end
 end
